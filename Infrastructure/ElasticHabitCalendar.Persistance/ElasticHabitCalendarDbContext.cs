1de17cd3286907b7f243328ba4be19fc00873b1f
sebastianappler <sebastian.appler@gmail.com>
2020-04-27 22:10:48 +0200

    Added Persistance project

---
+++ Infrastructure/ElasticHabitCalendar.Persistance/ElasticHabitCalendarDbContext.cs
@@ -0,0 +1,12 @@
+﻿using ElasticHabitCalendar.Application.Common.Interfaces;
+using ElasticHabitCalendar.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ElasticHabitCalendar.Persistance
+{
+    public class ElasticHabitCalendarDbContext : IElasticHabitCalendarDbContext
+    {
+        public DbSet<Habit> Habits { get; set; }
+        public DbSet<HabitCompletion> HabitCompletions { get; set; }
+    }
+}
