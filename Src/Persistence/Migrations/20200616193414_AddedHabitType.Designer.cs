2ffc18e87b51fb5a59b01a345eb4d042dbd6b729
sebastianappler <sebastian.appler@gmail.com>
2020-06-16 22:27:49 +0200

    Added HabitType migrations

--- Src/Persistence/Migrations/20200616193414_AddedHabitType.Designer.cs
+++ Src/Persistence/Migrations/20200616193414_AddedHabitType.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using EverydayHabit.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EverydayHabit.Persistence.Migrations
 {
     [DbContext(typeof(EverydayHabitDbContext))]
-    partial class EverydayHabitDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200616193414_AddedHabitType")]
+    partial class AddedHabitType
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
