3b6f1f70bd3fbbe7d64d8143ccceae2dcf944bbf
sebastianappler <sebastian.appler@gmail.com>
2020-07-11 21:17:19 +0200

    Changed to smoother splash screen.

--- Src/Targets/EverydayHabit.Android/MainActivity.cs
+++ Src/Targets/EverydayHabit.Android/MainActivity.cs
@@ -1,18 +1,22 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using EverydayHabit.Android;
 
 namespace EverydayHabit
 {
-    [Activity(Theme = "@style/MainTheme",
+    [Activity(Theme = "@style/MainTheme.Splash", MainLauncher = true,
         ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
         {
+            SetTheme(Resource.Style.MainTheme);
+
             base.OnCreate(bundle);
             global::Xamarin.Forms.Forms.Init(this, bundle);
             global::Xamarin.Forms.FormsMaterial.Init(this, bundle);
+
             LoadApplication(new App());
         }
     }
