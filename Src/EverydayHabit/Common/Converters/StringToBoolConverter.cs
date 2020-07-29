a91793e63cd7935dbd0f59c7890adbed95df5b19
sebastianappler <13065474+sebastianappler@users.noreply.github.com>
2020-07-29 22:21:52 +0200

    Hide icon from list when not set.

---
+++ Src/EverydayHabit/Common/Converters/StringToBoolConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace EverydayHabit.XamarinApp.Common.Converters
+{
+    public class StringToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !string.IsNullOrEmpty((string)value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (string) value;
+        }
+    }
+}
